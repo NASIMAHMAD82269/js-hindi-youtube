832984341885fb9db74f70bb8b9f91e8cc3743a4
NASIM AHMAD <nasimahmad82269@gmail.com>
2025-07-08 07:05:51 +0000

    datatypes

---
+++ 01-basics/02-dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all js code as newer version
+
+// alert("hello") we are using nodejs, not browser
+
+console.log(6 +  6)
+console.log("Nasim Ahmad")
+
+let name = "Nasim Ahmad"
+let age = 20
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint =>
+// string =>""
+// boolean => true / false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof age);
