a1e9ffd8b8d3f9a81ad81ea47de9670ddb43368a
NASIM AHMAD <nasimahmad82269@gmail.com>
2025-07-08 07:08:30 +0000

    datatypes

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,19 @@
+const accountId = 15552
+let accountEmail ="nasim@google.com"
+var accountPassword ="1203"
+accountCity ="Ramnagar"
+
+// accountId = 3
+
+accountEmail = "nasim.com"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+
+*/
+
+
+console.table([accountEmail,accountPassword,accountId,accountCity])
